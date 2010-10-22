bf488fc8c1dd70df40e0e5814af55b6cba7a6d7f
Moritz Schmale <narrow.m@gmail.com>
2010-10-23 00:03:13 +0200

    Rotate items to 90 steps

--- graphicsview.cpp
+++ graphicsview.cpp
@@ -20,18 +20,32 @@ bool GraphicsView::gestureEvent(QGestureEvent *event){
     if(event->gesture(Qt::PinchGesture)!=0){
 	QPinchGesture* gesture=static_cast<QPinchGesture*>(event->gesture(Qt::PinchGesture));
 	QPinchGesture::ChangeFlags changeFlags = gesture->changeFlags();
-//	if (changeFlags & QPinchGesture::RotationAngleChanged) {
-//	    qreal value = gesture->property("rotationAngle").toReal();
-//	    qreal lastValue = gesture->property("lastRotationAngle").toReal();
-//	    rotationAngle += value - lastValue;
-//	}
+	if (changeFlags & QPinchGesture::RotationAngleChanged) {
+	    qreal value = gesture->property("rotationAngle").toReal();
+	    qreal lastValue = gesture->property("lastRotationAngle").toReal();
+	    foreach(QGraphicsItem*i, scene()->selectedItems()){
+		qreal angle=i->data(QGraphicsItem::UserType+1411).toReal()+value-lastValue;
+		i->setData(QGraphicsItem::UserType+1411,angle);
+		i->rotate(value-lastValue);
+	    }
+	}
 	if (changeFlags & QPinchGesture::ScaleFactorChanged) {
 	    qreal value = gesture->property("scaleFactor").toReal();
-	    currentStepScaleFactor = value;
+	    if(qAbs(1.0-value)>0.05){
+		currentStepScaleFactor = value;
+	    }
 	}
 	if (gesture->state() == Qt::GestureFinished) {
 	    scaleFactor *= currentStepScaleFactor;
 	    currentStepScaleFactor = 1;
+	    foreach(QGraphicsItem*i, scene()->selectedItems()){
+		qreal rotation=i->data(QGraphicsItem::UserType+1411).toReal();
+		qreal r=round(rotation/90);
+		r*=90;
+		qDebug()<<"rotation"<<rotation<<"final rotation"<<r;
+		i->rotate(r-rotation);
+		i->setData(QGraphicsItem::UserType+1411,r);
+	    }
 	}
 	setScale(scaleFactor);
     }
