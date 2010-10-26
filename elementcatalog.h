5288153fa026f72dddbc56ee9ac8df86c19e9519
Moritz Schmale <narrow.m@gmail.com>
2010-10-26 13:11:28 +0200

    Removed obsolete code

--- elementcatalog.h
+++ elementcatalog.h
@@ -6,9 +6,6 @@
 #include <QAbstractItemDelegate>
 #include "catalogitem.h"
 
-//#define USE_OLD_ELEMENTCATALOG
-
-#ifndef USE_OLD_ELEMENTCATALOG
 //Second Implementation: more flexible and with tree support
 class ElementCatalog : public QAbstractItemModel
 {
@@ -44,41 +41,5 @@ private:
 	Qt::DropActions supportedDropActions() const;
     QStringList mimeTypes() const;
 };
-
-#else
-
-class ElementCatalog : public QAbstractListModel
-{
-    Q_OBJECT
-public:
-    explicit ElementCatalog(QObject *parent = 0);
-	~ElementCatalog();
-    int rowCount(const QModelIndex &parent) const;
-    QVariant data(const QModelIndex &index, int role) const;
-	bool setData(const QModelIndex &index, const QVariant &value, int role);
-    void addData(QString name, QString value, int row = -1);
-    QMimeData* mimeData(const QModelIndexList &indexes) const;
-    Qt::ItemFlags flags(const QModelIndex &index) const;
-    bool dropMimeData(const QMimeData *data, Qt::DropAction action, int row, int column, const QModelIndex &parent);
-    Qt::DropActions supportedDropActions() const;
-    QStringList mimeTypes() const;
-	bool removeRows(int row, int count, const QModelIndex &parent);
-	void load(QString file="");
-	void save(QString file="");
-signals:
-
-public slots:
-private:
-    QList< QPair<QString,QString> > myData;
-};
-
-//class ElementDelegate : public QAbstractItemDelegate
-//{
-//	Q_OBJECT
-//public:
-//	explicit ElementDelegate(QObject*parent=0);
-//	QWidget* createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const;
-//};
-#endif
 
 #endif // ELEMENTCATALOG_H
